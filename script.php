f46a9eda0c11cfcb8e64e9077e1ae18479d58520
joykep10 <mportillac@misena.edu.co>
2021-06-15 23:59:25 -0500

    esta es la version 0

---
+++ script.php
@@ -0,0 +1,8 @@
+<?php 
+	//var_dump($_GET);
+	echo $_GET['user']."<br>";
+	echo $_GET['password']."<br>";
+// ejempos diferentas
+	echo $_POST['user1']."<br>";
+	echo $_POST['password1']."<br>";
+?>
